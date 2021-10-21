b2417445a6d604b1bf0a86f5617718f99b62bd7b
PRIGENT Sylvain <sylvain.prigent@inria.fr>
2021-10-21 17:13:47 +0200

    fix bug in mac scirpts

--- mac/data_processing.sh
+++ mac/data_processing.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-install_path="/Applications/BioimageIT"
+install_path="/Applications/BioimageIT.app"
 "$install_path/miniconda3/envs/bioimageit/bin/python" "$install_path/bioimageit_gui/finderapp.py"
