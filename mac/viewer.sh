efd0c3854de4c1b447dd65379b17d793bfe42f20
PRIGENT Sylvain <sylvain.prigent@inria.fr>
2021-10-26 14:37:25 +0200

    update viewer script for mac

--- mac/viewer.sh
+++ mac/viewer.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 install_path="/Users/$USER/BioimageIT"
-"$install_path/miniconda3/envs/bioimageit/bin/python" "$install_path/bioimageit_gui/biviewerapp.py" $1
+"$install_path/miniconda3/envs/bioimageit/bin/python" "$install_path/bioimageit_viewer/biviewerapp.py" $1
