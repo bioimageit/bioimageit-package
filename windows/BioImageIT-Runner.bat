3a80f888fc7c79ce916c22c4a0f43c27a64e290b
leO9764 <leomaury97@gmail.com>
2021-11-03 10:42:39 +0100

    modification ofinstall path

--- windows/BioImageIT-Runner.bat
+++ windows/BioImageIT-Runner.bat
@@ -1,8 +1,8 @@
-set conda_path="C:\Users\%USERNAME%\BioimageIT_installation_files\Miniconda3\condabin\conda.bat"
-set miniconda_path=C:\Users\"%USERNAME%"\BioimageIT_installation_files\Miniconda3
+set conda_path="C:\Users\%USERNAME%\BioImageIT\Miniconda3\condabin\conda.bat"
+set miniconda_path=C:\Users\"%USERNAME%"\BioImageIT\Miniconda3
 
 call %miniconda_path%\Scripts\activate.bat bioimageit
 
 cd %cd%
 
-python "C:\Users\%USERNAME%\BioimageIT_installation_files\bioimageit_gui\runnerapp.py" %1
+python "C:\Users\%USERNAME%\BioImageIT\bioimageit_gui\runnerapp.py" %1
--- windows/BioImageIT-Toolboxes.bat
+++ windows/BioImageIT-Toolboxes.bat
@@ -1,6 +1,6 @@
-set conda_path="C:\Users\%USERNAME%\BioimageIT_installation_files\Miniconda3\condabin\conda.bat"
-set miniconda_path=C:\Users\"%USERNAME%"\BioimageIT_installation_files\Miniconda3
+set conda_path="C:\Users\%USERNAME%\BioImageIT\Miniconda3\condabin\conda.bat"
+set miniconda_path=C:\Users\"%USERNAME%"\BioImageIT\Miniconda3
 
 call %miniconda_path%\Scripts\activate.bat bioimageit
 
-python "C:\Users\%USERNAME%\BioimageIT_installation_files\bioimageit_gui\finderapp.py"
+python "C:\Users\%USERNAME%\BioImageIT\bioimageit_gui\finderapp.py"
