dd6d1b92f758392b867dc16c69c5028e8ec1a271
leO9764 <42140279+leO9764@users.noreply.github.com>
2021-12-06 11:03:24 +0100

    Update BioImageIT-Viewer.sh

--- linux/BioImageIT-Viewer.sh
+++ linux/BioImageIT-Viewer.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 install_path="/home/$USER/BioImageIT"
-"$install_path/miniconda3/envs/bioimageit/bin/python" "$install_path/bioimageit_gui/biviewerapp.py" $1
+"$install_path/miniconda3/bin/python" "$install_path/bioimageit_gui/biviewerapp.py" $1
