1b93cd306f2604af473823ee97594e6628c9e272
PRIGENT Sylvain <sylvain.prigent@inria.fr>
2022-03-16 12:25:57 +0100

    add jupyter app for mac

--- mac/BioImageIT-Runner.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-install_path="/Users/$USER/BioimageIT"
-"$install_path/miniconda3/envs/bioimageit/bin/python" "$install_path/bioimageit_gui/runnerapp.py" $1
--- mac/jupyter.sh
+++ mac/jupyter.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 install_path="/Users/$USER/BioimageIT"
-"$install_path/miniconda3/envs/bioimageit/bin/jupyter" notebook
+"$install_path/miniconda3/envs/bioimageit/bin/jupyter" notebook --notebook-dir "$install_path/BioImageIT-notebooks"
