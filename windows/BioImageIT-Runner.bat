38d080a92e2781361022b1b927f23829064257a8
leO9764 <42140279+leO9764@users.noreply.github.com>
2022-03-16 11:59:54 +0100

    Delete BioImageIT-Runner.bat

--- windows/BioImageIT-Runner.bat
+++
@@ -1,8 +0,0 @@
-set conda_path="C:\Users\%USERNAME%\BioImageIT\Miniconda3\condabin\conda.bat"
-set miniconda_path=C:\Users\"%USERNAME%"\BioImageIT\Miniconda3
-
-call %miniconda_path%\Scripts\activate.bat bioimageit
-
-cd %cd%
-
-python "C:\Users\%USERNAME%\BioImageIT\bioimageit_gui\runnerapp.py" %1
