e4faf9ed2b084ecbb03afb95f5ec4cf20f253d4c
leO9764 <42140279+leO9764@users.noreply.github.com>
2022-03-16 12:00:26 +0100

    Delete BioImageIT-Viewer.bat

--- windows/BioImageIT-Viewer.bat
+++
@@ -1,6 +0,0 @@
-set conda_path="C:\Users\%USERNAME%\BioImageIT\Miniconda3\condabin\conda.bat"
-set miniconda_path=C:\Users\"%USERNAME%"\BioImageIT\Miniconda3
-
-call %miniconda_path%\Scripts\activate.bat bioimageit
-
-python "C:\Users\%USERNAME%\BioImageIT\bioimageit_viewer\biviewerapp.py" %1
