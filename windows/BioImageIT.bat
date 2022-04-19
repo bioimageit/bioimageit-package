2737a3bde64caf0a18b5d108d7a58088c899b614
leO9764 <42140279+leO9764@users.noreply.github.com>
2022-04-19 09:46:25 +0200

    changed paths

--- windows/BioImageIT.bat
+++ windows/BioImageIT.bat
@@ -1,6 +1,6 @@
-set conda_path="C:\Users\%USERNAME%\BioImageIT\Miniconda3\condabin\conda.bat"
-set miniconda_path=C:\Users\"%USERNAME%"\BioImageIT\Miniconda3
+set conda_path=".\Miniconda3\condabin\conda.bat"
+set miniconda_path=.\Miniconda3
 
 call %miniconda_path%\Scripts\activate.bat bioimageit
 
-python "C:\Users\%USERNAME%\BioImageIT\bioimageit_gui\app.py"
+python ".\bioimageit_gui\app.py"
